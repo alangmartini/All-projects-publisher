1f463f17a56e65c835457c4c805d98582db24d0c
gmaralan <gmartinialan@gmail.com>
2023-02-11 11:51:55 -0400

    added colorful logs and eslint

--- main.mjs
+++ main.mjs
@@ -1,7 +1,10 @@
+/* eslint-disable max-len */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
 import { promisify } from 'util';
 import { exec, spawn } from 'child_process';
-import _ from 'lodash';
-import inquirer from 'inquirer'; 
+import inquirer from 'inquirer';
+import { logGreenBigBold, logRedBigBold, logYellowBigBold } from './colorfulLogs/logs.njs';
 // Question imports
 import branchesStardartQuestion from './inquirerQuestions/branchHandling/isBranchesStardartQuestion.mjs';
 import questionBranchNameFromInput from './inquirerQuestions/branchHandling/branchNameUnique.mjs';
@@ -14,7 +17,6 @@ import standartBranchNameQuestion from './inquirerQuestions/branchHandling/stard
 import userNameForRepoNameQuestion from './inquirerQuestions/projectNameHandling/userNameForRepoNameQuestion.mjs';
 
 const asyncExec = promisify(exec);
-const spawnPromise = promisify(spawn)
 
 const query = `gh api graphql --paginate -f query='
 query($endCursor: String) { 
@@ -32,12 +34,12 @@ query($endCursor: String) {
 
 async function fetchProjects(triboAtual) {
   let repositories;
-  try { 
+  try {
     const { stdout, stderr } = await asyncExec(query);
     // const reg = new RegExp(`sd-0${triboAtual}-[ab]-project.*?(?="},)`, 'g');
     const reg = new RegExp(`sd-0${triboAtual}-[ab].*?(?="},)`, 'g');
     const matchIterator = stdout.matchAll(reg);
-    
+
     const repositoriesArr = [];
     for (const match of matchIterator) {
       repositoriesArr.push(match[0]);
@@ -46,49 +48,50 @@ async function fetchProjects(triboAtual) {
     repositories = repositoriesArr;
 
     if (stderr) {
-      throw new Error(stderr)
-    }      
+      throw new Error(stderr);
+    }
   } catch (err) {
     console.error(err);
   }
 
   return repositories;
-};
+}
 
 async function cloneRepository(repository) {
-  const { stdout, stderr } = await asyncExec('ls');
+  const { stdout } = await asyncExec('ls');
   const isProjectExistent = stdout.match(repository);
 
   if (isProjectExistent) {
-    console.log('Encontrado projeto na sua pasta!');
+    logGreenBigBold('Encontrado projeto na sua pasta!');
   } else {
-    const cloneURL = 'git@github.com:tryber/' + repository + '.git';
+    logYellowBigBold(`Clonando ${repository}`);
+    const cloneURL = `git@github.com:tryber/${repository}.git`;
     await asyncExec(`git clone ${cloneURL}`);
   }
 }
 
-async function runCommand(command) {
-  const child = await spawnPromise(command, { shell: true });
-  // Write command info to log
-  child.stdout.on('data', (data) => console.log(data.toString()));
-  // Write errors to log
-  child.stderr.on('data', (data) => console.error(data.toString()));
+async function deleteRepository(repository) {
+  try {
+    logYellowBigBold('DELETANDO REPOSITORIO CLONADO');
+    await asyncExec(`rm -rf ${repository}`);
+  } catch (e) {
+    logRedBigBold('Algo deu errado ao deletar o repositorio:');
+    console.log(error.stdout);
+  }
+}
 
-  child.on('close', (code) => console.error(`Child closed with code ${code}`));
-};
-
-async function runPublisher(repository, 
+async function runPublisher(
+  repository,
   hasStandartBranch,
   declareNameForProject,
   userName,
-  ) {
-
+) {
   // If there is no standartBranchName, get one from input.
   let branchForCurrentRepository;
   if (!hasStandartBranch) {
     // Get current project branchName from input
-    const { branchNameFromInput } = await inquirer.prompt(questionBranchNameFromInput(repository));
-    
+    const { branchNameFromInput } = await inquirer
+      .prompt(questionBranchNameFromInput(repository));
     branchForCurrentRepository = branchNameFromInput;
   } else {
     branchForCurrentRepository = hasStandartBranch;
@@ -97,48 +100,48 @@ async function runPublisher(repository,
   let projectName;
   if (declareNameForProject === 'Agora') {
     // Get new repository name
-    const { repoName } = await inquirer.prompt(questionRepositoryNameFromInput(repository));
+    const { repoName } = await inquirer
+      .prompt(questionRepositoryNameFromInput(repository));
     // To-do: format the name so its '-' split
     projectName = repoName;
   } else {
-    projectName = `${userName.split(' ').join('-')}-${projectRenames[repository].split(' ').join('-')}`;
+    projectName = `${userName
+      .split(' ').join('-')}-${projectRenames[repository].split(' ').join('-')}`;
   }
 
-  const asyncSpawn = async () => {
-    return new Promise((resolve, reject) => {
-      console.log(repository)
-      const publisherProcess = spawn(`trybe-publisher`, [`-b`, `${branchForCurrentRepository}`, `-p`, `${projectName}`], {
+  const asyncSpawn = async () => new Promise((resolve, reject) => {
+    console.log(repository);
+    const publisherProcess = spawn(
+      'trybe-publisher',
+      ['-b', `${branchForCurrentRepository}`, '-p', `${projectName}`],
+      {
         cwd: repository,
-        stdio: "inherit"
-      });
+        stdio: 'inherit',
+      },
+    );
 
-      publisherProcess.on('exit', (code) => {
-        if (code === 0) {
-          resolve()
-        } else {
-          reject(new Error(`trybe publisher exit with error ${code}`))
-        }
-      })
+    publisherProcess.on('exit', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`trybe publisher exit with error ${code}`));
+      }
     });
-  } 
+  });
 
   try {
-    // const { stdout,
-    //   stderr } = await runCommand(`cd ${repository} && trybe-publisher -b "${branchForCurrentRepository}" -p "${projectName}"`);
-      await asyncSpawn();
-      console.log(`Finished publishing ${repository} as ${projectName}`)
-  } 
-  catch(e) {
-    console.log(e.error)
+    await asyncSpawn();
+    console.log(`Finished publishing ${repository} as ${projectName}`);
+  } catch (e) {
+    console.log('Aconteceu algum erro! ');
+    console.log(e.error);
   }
-
 }
 
 async function main() {
-
   const { areBranchesStandartized } = await inquirer.prompt(branchesStardartQuestion);
-  console.log(areBranchesStandartized)
-  
+  console.log(areBranchesStandartized);
+
   let standartBranchName;
   if (areBranchesStandartized === 'Sim') {
     const { _standartBranchName } = await inquirer.prompt(standartBranchNameQuestion);
@@ -147,41 +150,29 @@ async function main() {
 
   const { currentTrybe } = await inquirer.prompt(currentTrybeQuestion);
 
-  const { useDefaultNameForProjects } = await inquirer.prompt(useDefaultNameForProjectsQuestion); 
+  const { useDefaultNameForProjects } = await inquirer
+    .prompt(useDefaultNameForProjectsQuestion);
 
   let userName;
-  if ( useDefaultNameForProjects === 'Depois') {
-    const { userNameForRepo } = await inquirer.prompt(userNameForRepoNameQuestion)
+  if (useDefaultNameForProjects === 'Depois') {
+    const { userNameForRepo } = await inquirer.prompt(userNameForRepoNameQuestion);
     userName = userNameForRepo;
-  };
+  }
 
-  console.log('Pegando todos os projetos da sua turma atualmente disponíveis no GitHub')
+  console.log('Pegando todos os projetos da sua turma atualmente disponíveis no GitHub');
   const projectsFromCurrentTrybe = await fetchProjects(currentTrybe);
 
-  const { projectsToUpload } = await inquirer.prompt(whatProjectsToUploadQuestion(projectsFromCurrentTrybe));
+  const { projectsToUpload } = await inquirer
+    .prompt(whatProjectsToUploadQuestion(projectsFromCurrentTrybe));
 
-  console.log("Beleza! Agora começara o process de clonar o projeto, achar sua branch e renomear o projeto (caso tenha decido) para subi-lo em seu Github")
+  console.log(`Beleza! Agora começara o process de clonar o projeto, 
+  achar sua branch e renomear o projeto (caso tenha decido) para subi-lo em seu Github`);
 
-  for (let project of projectsToUpload) {
+  for (const project of projectsToUpload) {
     await cloneRepository(project);
     await runPublisher(project, standartBranchName, useDefaultNameForProjects, userName);
-    // await deleteClone(project)
+    await deleteRepository(project);
   }
 }
 
-await main()
-
-
-// publisherProcess.on("exit", code => {
-//   console.log(`trybe-publisher exited with code ${code}`);
-// });
-
-// try {
-//   const stAlgo = await asyncExec(`cd sd-026-b-project-lessons-learned && trybe-publisher -b "alan" -p "Testes-unitarios-jestt"`);
-
-//   console.log(stAlgo.stdout)
-// } 
-// catch(e) {
-//   console.log('Atenção: o seguinte erro foi gerado pelo trybe-publisher:')
-//   console.log(e.stdout)
-// }
+await main();
